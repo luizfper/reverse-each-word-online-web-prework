9b2d2989a75c3bbfd2718e3f8bf3e2a6781e264a
Luiz Pereira <luizfper@gmail.com>
2019-01-31 01:27:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(phrase)
   phrase_array=phrase.split
-  phrase_reversed=reverse_array(phrase_array).to_s
+  phrase_reversed=reverse_array(phrase_array).join(" ")
 end
 
 def reverse_array(array)
