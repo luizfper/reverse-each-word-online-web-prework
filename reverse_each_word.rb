6d4ba693fa5f65b272183afef13994d39a5de8d3
Luiz Pereira <luizfper@gmail.com>
2019-01-31 01:26:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(phrase)
   phrase_array=phrase.split
-  phrase_reversed=""
-  phrase_array.each do |word|
-    phrase_reversed = phrase_reversed << " " << word.reverse
+  phrase_reversed=reverse_array(phrase_array).to_s
+end
+
+def reverse_array(array)
+  array.collect do |word|
+    word.reverse
   end
-  phrase_reversed
 end
